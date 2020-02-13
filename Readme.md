2a557e10f65ee97d3882785f2f6ef442fae23736
Alex <ajdorrington@hotmail.co.uk>
2020-02-13 18:52:29 +0000

    Fourth commit - Data renders to frontend for get and post requests

--- Readme.md
+++ Readme.md
@@ -5,3 +5,6 @@ Node.js - Express
 Passport for auth
 bootstrap
 
+Todo next:
+ - Handle repeat post request for same date (override intial object)
+ - Render data to frontend
--- src/public/js/dashboard.js
+++ src/public/js/dashboard.js
@@ -1,15 +1,32 @@
+const dataBlocksContainer = document.getElementById('dataContainer')
+
+
 //FETCH WEIGHT DATA
 const fetchWeightData = async () => {
     const data = await fetch('http://localhost:3000/dashboard/weight', {
         method: 'GET'
     })
     const jsonData = await data.json()
-    console.log(jsonData)
+    renderWeightData(jsonData)
 }
 fetchWeightData()
 
-//DISPLAY WEIGHT DATA
 
+//RENDER EXISTING WEIGHT DATA ON LOAD
+const renderWeightData = (weightDataArr) => {
+    for (let weight of weightDataArr) {
+        const contentBody = document.createElement('span')
+        contentBody.innerHTML = `
+        <div class="card text-center" style="width: 15vw; height: 10vh; margin: 2px; padding: 2px; flex-direction: column;">
+            <div>
+              <h5 class="card-title">${weight.date}</h5>
+              <a href="#" class="card-link">Weight: ${weight.weight} ${weight.measure}</a>
+              <a href="#" class="card-link">Bodyfat: ${weight.bodyfat}%</a>
+            </div>
+        </div>`
+        dataBlocksContainer.appendChild(contentBody)
+    }
+}
 
 
 //POST NEW WEIGHT DATA
@@ -35,17 +52,34 @@ weightForm.addEventListener('submit', async (e) => {
     e.preventDefault()
     let btn;
     kgRadioBtn.checked ? btn = 'kg' : btn = 'lbs'
+    const weight = weightInput.value
+    const bodyfat = bodyfatInput.value
+    const date = dateInput.value
     await fetch('http://localhost:3000/dashboard/weight', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({
-            weight: weightInput.value,
+            weight,
             measure: btn,
-            bodyfat: bodyfatInput.value,
-            date: dateInput.value
+            bodyfat,
+            date
         })
     })
-    fetchWeightData()
+    renderNewWeight(weight, bodyfat, date, btn)
 })
+
+//RENDER NEWLY ADDED WEIGHT TO HTML
+const renderNewWeight = (weight, bodyfat, date, lbs) => {
+    const contentBody = document.createElement('span')
+    contentBody.innerHTML = `
+    <div class="card text-center" style="width: 15vw; height: 10vh; margin: 2px; padding: 2px; flex-direction: column;">
+        <div>
+            <h5 class="card-title">${date}</h5>
+            <a href="#" class="card-link">Weight: ${weight} ${lbs}</a>
+            <a href="#" class="card-link">Bodyfat: ${bodyfat}%</a>
+        </div>
+    </div>`
+    dataBlocksContainer.appendChild(contentBody)
+}
