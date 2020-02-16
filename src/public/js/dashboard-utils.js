ee3d8d1c3ee780fe86de3cb38122e75a58eb6a78
Alex <ajdorrington@hotmail.co.uk>
2020-02-16 12:38:06 +0000

    commit 13 - Started work on filter feature

--- src/public/js/dashboard-utils.js
+++ src/public/js/dashboard-utils.js
@@ -12,6 +12,40 @@ slideFilterBox.addEventListener('click', () => {
 })
 
 
+//CHECK AND UNCHECKED RADIO BUTTONS
+const dateRadioBtn = document.getElementById('filterDateRadio')
+const weightRadioBtn = document.getElementById('filterWeightRadio')
+const bodyfatRadioBtn = document.getElementById('filterBodyfatRadio')
+
+const radioBtnArray = [dateRadioBtn, weightRadioBtn, bodyfatRadioBtn]
+
+radioBtnArray.forEach(btn => {
+    btn.addEventListener('change', (e) => {
+        radioBtnArray.forEach((btn) => [
+            btn.checked = false
+        ])
+        e.target.checked = true
+    })
+})
+
+
+//SUBMIT FILTER
+const filterSubmitBtn = document.getElementById('submitFilter')
+
+filterSubmitBtn.addEventListener('click', () => {
+    let filterBy;
+    radioBtnArray.forEach((btn) => {
+        if (btn.checked) {
+            filterBy = btn.value
+        }
+    })
+    const test = weightData.filter((item) => {
+        return item[filterBy] == 171
+    })
+    console.log(test)
+})
+
+
 //CLOSE MODAL
 closeModalBtn.addEventListener('click', () => {
     dataAddedTip.style.display = 'none'
