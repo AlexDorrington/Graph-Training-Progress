e48d9a085ebe584dba25cc593cb94d3da332684f
Alex <ajdorrington@hotmail.co.uk>
2020-02-20 20:41:56 +0000

    commit 26 - Added basic calendar feature

--- src/public/js/exercises.js
+++ src/public/js/exercises.js
@@ -16,17 +16,35 @@ const calendar = [
 
 //RENDER DAYS PER MONTH DIV
 const monthBtnsDiv = document.getElementById('monthBtns')
+const daysContainer = document.getElementById('daysContainer')
 
 monthBtnsDiv.addEventListener('click', (e) => {
-    renderDays(e.target.name)
+    daysContainer.innerHTML = ''
+    renderBtnsInactive()
+    e.target.classList.add('activeMonth')
+    const btnMonthName = e.target.name
+    returnNoOfDays(btnMonthName, (day) => {
+        const newButton = document.createElement('button')
+        newButton.classList.add('dayBtn')
+        newButton.innerHTML = day
+        daysContainer.appendChild(newButton)
+    })
 })
 
-const renderDays = (btnName) => {
+const renderBtnsInactive = () => {
+    const btns = Array.from(document.querySelectorAll('.month'))
+    btns.forEach(btn => {
+        btn.classList.remove('activeMonth')
+    })
+}
+
+const returnNoOfDays = (btnName, cb) => {
     const btnMonth = btnName
     const monthFromCalendarArry = calendar.find((month) => month[0].toLowerCase() == btnMonth)
-    let i = 0;
+    let i = 1;
     do {
-        console.log(i)
+        cb(i)
         i++
     } while (i <= monthFromCalendarArry[1])
 }
+
